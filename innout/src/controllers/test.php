ca237f335ffa5bacedec9e0af8abf632dd3e800c
GuilhermeTrindadeV <guiviveiro@gmail.com>
2023-01-30 12:11:56 -0300

    registar ponto

---
+++ innout/src/controllers/test.php
@@ -0,0 +1,4 @@
+<?php
+// Controller Temporário!
+
+echo User::getCount(['raw' => 'id % 2 = 0']);
---
+++ innout/src/views/template/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div>
+        <span>Desenvolvido com</span>
+        <span><i class="icofont-heart text-primary mx-1"></i></span>
+        <span>por Guilherme <span class="text-primary">Trindade</span></span>
+    </div>
+</footer>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
